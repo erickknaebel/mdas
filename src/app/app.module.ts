46683084003cedace1da3e40b7848fc3ab8b0cee
Erick Knaebel <eknaeb@roushnet.com>
2020-02-12 12:52:17 -0500

    code clean up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,12 +14,12 @@ import { AngularFirestore } from '@angular/fire/firestore';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
 import { environment } from 'src/environments/environment';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
