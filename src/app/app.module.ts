e12f2dc2cd93da7c9f3856ed81faac80060de435
Erick Knaebel <eknaeb@roushnet.com>
2020-02-12 12:01:55 -0500

    install angular material and firebase in project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,54 @@
+// Angular Material Compnents
+import {
+  MatButtonModule, MatNativeDateModule, MatDatepickerModule, MatToolbarModule,
+  MatListModule, MatCheckboxModule, MatInputModule, MatSelectModule, MatSliderModule,
+  MatButtonToggleModule, MatStepperModule, MatTableModule, MatIconModule
+} from '@angular/material';
+
+// Angular Firebase
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { AngularFirestore } from '@angular/fire/firestore';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AngularFireAuthModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    BrowserAnimationsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    MatSelectModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatListModule,
+    MatToolbarModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatButtonModule,
+    MatSliderModule,
+    MatButtonToggleModule,
+    MatStepperModule,
+    MatTableModule,
+    MatIconModule,
   ],
-  providers: [],
+  providers: [AngularFireDatabase, AngularFirestore],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
