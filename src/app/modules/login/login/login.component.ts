64b0929208d0d3c4767ef4459dc759ba96e473c5
Erick Knaebel <eknaeb@roushnet.com>
2020-02-12 19:53:09 -0500

    code and directory clean up

--- src/app/modules/login/login/login.component.ts
+++ src/app/modules/login/login/login.component.ts
@@ -1,12 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFireAuth } from '@angular/fire/auth';
-import { Router } from '@angular/router';
-import { Store } from '@ngrx/store';
-import { AppState } from 'src/app/app.state';
-import * as LoginActions from './../../../ngrx/actions/login.actions'
-import { Login } from '../../../ngrx/models/login.models';
-import { EmailValidator } from '@angular/forms';
-
 
 @Component({
   selector: 'app-login',
@@ -15,27 +7,7 @@ import { EmailValidator } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
 
-  constructor( private router: Router, private firebase: AngularFireAuth, private store: Store<AppState>) { }
-
-  userData$ = this.store.select('user').subscribe(value => {
-    console.log(value)
-  })
-
-
-  login(email: string, password: string) {
-    this.firebase
-      .auth
-      .signInWithEmailAndPassword(email, password)
-      .then(res => {
-        console.log('Successfully signed in!');
-        sessionStorage.setItem('authenticated', 'true');
-        this.store.dispatch(new LoginActions.AppLogin({username: email, password: password, photoUrl: ''}) )
-        this.router.navigate(['dashboard']);
-      })
-      .catch(err => {
-        console.log('Something is wrong:', err.message);
-      });
-  }
+  constructor() { }
 
   ngOnInit() {
   }
